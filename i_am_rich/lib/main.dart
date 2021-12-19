827e180aef209ebc76c0d495d4f39f159fa26b7b
krishi-trip <66849513+krishi-trip@users.noreply.github.com>
2021-12-18 19:23:33 -0500

    finished app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,10 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage("assets/diamond.png"),
+          ),
+        ),
       ),
     ),
   );
