186a358dac77678d350e4460e48cb6a680528ffe
krishi-trip <66849513+krishi-trip@users.noreply.github.com>
2022-01-13 07:45:58 -0500

    Testing code

--- Java/Test.java
+++ Java/Test.java
@@ -1,8 +1,6 @@
 public class Test{
 	public static void main(String[] args) {
-		System.out.println("This is a simple print statement");
-		System.out.println("This is a second print statement");
-		System.out.println("This is a third print statement");
-		System.out.println("This is a fourth print statement");
+		String s = null; 
+		System.out.println(s);
 	}	
 }
