b37248aeb0d0ecfd6f94835993383f605fbbdc12
krishi-trip <66849513+krishi-trip@users.noreply.github.com>
2022-01-25 14:08:01 -0500

    Synced

--- Java/src/PracticeWithVim.java
+++ Java/src/PracticeWithVim.java
@@ -1,6 +1,6 @@
 public class PracticeWithVim{
-	public static void main(String[] args) {
-		
-		System.out.println("This was written in vim");
-	}
+    public static void main(String[] args) {
+	
+	System.out.println("This was written in vim");
+    }
 }
