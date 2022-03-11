03dacf609f00c9b06e6209450f27ceea561dfe2b
krishi-trip <66849513+krishi-trip@users.noreply.github.com>
2022-03-11 10:43:03 -0500

    Testing some code

--- Java/src/cs1302/visibility/Program1.java
+++ Java/src/cs1302/visibility/Program1.java
@@ -2,6 +2,9 @@ package cs1302.visibility;
 
 public class Program1 {
     public static void main(String[] args) {
+        int num, num2;
+
+        num = num2 = 0;
         System.out.println("Hello World");
     }
 }
